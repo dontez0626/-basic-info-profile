e3e5f8d8a9dae1683a6f85cee035ac04b7a55aed
G.Parker <82189292+dontez0626@users.noreply.github.com>
2021-07-04 08:32:29 -0400

    small fixes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /** @format */
 const fs = require("fs");
 const inquirer = require("inquirer");
-const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-
+const Engineer = require("./lib/Engineer");
 const employees = [];
 
 function initApp() {
